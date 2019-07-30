e2f06e0e16fc7cc38a5cd0bfad18e8465800bca9
Faisal Ahmed <faisal.ahmedbcse@gmail.com>
2019-07-30 12:46:42 +0600

    React Js Learning

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, {Compon} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import First from './components/First'
 
 function App() {
-  let p ='Faisal Ahmed haahha';
 
   return (
     <div className="App">
-      <header className="App-header">
-       <h2>{p?p:'No Data'}</h2>
-    
-      </header>
+
+      <First name=" Faisal Ahmed" email="faisal.ahmed@gmail.com"
+        address="Dhanmondi" />
+
     </div>
   );
 }
