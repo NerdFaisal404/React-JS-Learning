de405ef75dda5e0b082d59a064ad88bf2ed01e96
Faisal Ahmed <faisal.ahmedbcse@gmail.com>
2019-07-30 16:05:02 +0600

    worked with handler

--- src/App.js
+++ src/App.js
@@ -6,34 +6,62 @@ import Counter from './components/counter/Counter'
 
 class App extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      persons: [
-        { name: 'Faisal Ahmed', email: 'faisal.ahmedbcse@gmail.com', address: 'Dhanmondi' },
-        { name: 'Faisal Ahmed2', email: 'faisal.ahmedbcse2@gmail.com', address: 'Dhanmondi 2' },
-        { name: 'Faisal Ahmed3', email: 'faisal.ahmedbcse3@gmail.com', address: 'Dhanmondi 3' }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     persons: [
+  //       { name: 'Faisal Ahmed', email: 'faisal.ahmedbcse@gmail.com', address: 'Dhanmondi' },
+  //       { name: 'Faisal Ahmed2', email: 'faisal.ahmedbcse2@gmail.com', address: 'Dhanmondi 2' },
+  //       { name: 'Faisal Ahmed3', email: 'faisal.ahmedbcse3@gmail.com', address: 'Dhanmondi 3' }
 
-      ]
-    }
+  //     ]
+  //   }
+  // }
+
+  state = {
+
+    name: '',
+
+    persons: [
+      { name: 'Faisal Ahmed', email: 'faisal.ahmedbcse@gmail.com', address: 'Dhanmondi' },
+      { name: 'Faisal Ahmed2', email: 'faisal.ahmedbcse2@gmail.com', address: 'Dhanmondi 2' },
+      { name: 'Faisal Ahmed3', email: 'faisal.ahmedbcse3@gmail.com', address: 'Dhanmondi 3' }
+
+    ]
   }
 
+
+
+  clickHandler = (event) => {
+    console.log(event.target);
+  }
+
+  inputHandler = (event) => {
+    //console.log(event.target.value);
+    this.setState({
+      name: event.target.value
+    });
+  }
 
   render() {
 
     return (
       <div className="App">
 
-        {this.state.persons.map((item, key) =>
+        {/* {this.state.persons.map((item, key) =>
           <First name={item.name} email={item.email}
             address={item.address} />
-        )}
+        )} */}
+        <div className="container my-3">
 
-        {/* <First name={this.state.person[0].name} email={this.state.person[0].email}
-          address={this.state.person[0].address} />
+          <input onChange={this.inputHandler} type="text" placeholder="Enter Your Name" />
 
-        <Counter /> */}
-      </div>
+          <button className="btn btn-primary" onClick={this.clickHandler}>
+            Click Me
+        </button>
+          {this.state.name ? <p>Hello .. {this.state.name}</p> : ''}
+        </div >
+      </div >
     );
   }
 }
