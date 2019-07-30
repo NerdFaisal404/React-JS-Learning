985ec659a660d8b165620a6ceaad9cc4d2113101
Faisal Ahmed <faisal.ahmedbcse@gmail.com>
2019-07-30 15:25:46 +0600

    state added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import First from './components/First'
 import Counter from './components/counter/Counter'
 
-function App() {
+class App extends Component {
 
-  return (
-    <div className="App">
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Faisal Ahmed', email: 'faisal.ahmedbcse@gmail.com', address: 'Dhanmondi' },
+        { name: 'Faisal Ahmed2', email: 'faisal.ahmedbcse2@gmail.com', address: 'Dhanmondi 2' },
+        { name: 'Faisal Ahmed3', email: 'faisal.ahmedbcse3@gmail.com', address: 'Dhanmondi 3' }
 
-      <First name=" Faisal Ahmed" email="faisal.ahmed@gmail.com"
-        address="Dhanmondi" />
+      ]
+    }
+  }
 
-      <Counter />
-    </div>
-  );
+
+  render() {
+
+    return (
+      <div className="App">
+
+        {this.state.persons.map((item, key) =>
+          <First name={item.name} email={item.email}
+            address={item.address} />
+        )}
+
+        {/* <First name={this.state.person[0].name} email={this.state.person[0].email}
+          address={this.state.person[0].address} />
+
+        <Counter /> */}
+      </div>
+    );
+  }
 }
+
 
 export default App;
